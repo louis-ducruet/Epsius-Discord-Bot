bb3f50a7da808b78782c042d8b4484e80f6a7569
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-06-30 17:09:27 +0200

    Création commande ping #11

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,7 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: 'ping',
     description: 'Commande ping!',
-    runSlash(client, interaction) {
-        interaction.reply({content: 'Pong!', ephemeral: true});
+    async runSlash(client, interaction) {
+        const tryPong = await interaction.reply({ content: 'Chargement des données ...', ephemeral: true, fetchReply: true});
+        const tryPongTimestamp = new Date(tryPong.timestamp).getTime();
+        const embed = new MessageEmbed()
+            .setTitle('ℹ Info sur le bot')
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor('#3a86c0')
+            .addFields(
+                { name: 'Latence API', value: `\`\`\`${client.ws.ping}ms\`\`\``, inline: true },
+                { name: 'Latence BOT', value: `\`\`\`${tryPongTimestamp - interaction.createdTimestamp}ms\`\`\``, inline: true },
+                { name: 'BOT Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: false }
+            )
+            .setTimestamp()
+            .setFooter({
+                text: interaction.user.username,
+                iconURL: interaction.user.displayAvatarURL()
+            });
+
+        interaction.editReply({ content: null, embeds: [embed] });
     }
 }
