8db800f332ac530038156307f95cdd76d7d9f65f
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-06-30 15:52:29 +0200

    Gestion des erreurs #9

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ logger.info('---- Importation ----')
 const handlers = ['CommandUtil', 'EventUtil']
 handlers.forEach(handler => { require(`./utils/handlers/${handler}`)(client) });
 
+process.on('uncaughtException', (err, origin) => logger.error(`uncaughtException : ${err}\n Origine : ${origin}`));
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`unhandledRejection : ${reason}\n Promise :`)
+    console.log(promise);
+});
+process.on('exit', code => logger.warn(`Le processus s'est arrêté avec le code : ${code}`));
+process.on('warning', (...args) => console.log(...args));
+
 client.login(process.env.DISCORD_TOKEN);
