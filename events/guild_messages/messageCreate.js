96855e1852affc7f8cb9a93091a5202131475d73
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-07-06 16:40:27 +0200

    Fix message auto supp #37

--- events/guild_messages/messageCreate.js
+++ events/guild_messages/messageCreate.js
@@ -2,7 +2,8 @@ module.exports = {
     name: "messageCreate",
     once: false,
     async execute(client, message) {
-        if (process.env.DISCORD_RESTRICTED_CHANNELS.indexOf(message.channel.id) && !(message.member.id === '990896605957881897' || message.member.roles.cache.has(process.env.DISCORD_EXCEPTION_RESTRICTED_ROLE))){
+        const secureChannel = process.env.DISCORD_RESTRICTED_CHANNELS.indexOf(message.channel.id) === -1 ? false : true;
+        if (secureChannel && !(message.member.id === '990896605957881897' || message.member.roles.cache.has(process.env.DISCORD_EXCEPTION_RESTRICTED_ROLE))){
             message.delete();
         }
     }
