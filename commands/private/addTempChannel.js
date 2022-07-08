7aa6e3f97a3193e8f1f69d49c522aaa13db98d38
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-07-08 17:34:56 +0200

    Rendre plus flexible code par rapport classe #59

--- commands/private/addTempChannel.js
+++ commands/private/addTempChannel.js
@@ -1,3 +1,11 @@
+const choices = [{ name: 'Tous', value: 0 }];
+for (let i = 0; i < process.envVar.classes.length; i++) {
+    choices.push({ name: process.envVar.classes[i].nom, value: (i + 1) * 10 });
+    for (let j = 0; j < process.envVar.classes[i].groupesRole.length; j++) {
+        choices.push({ name: `${process.envVar.classes[i].nom} Groupe ${j + 1}`, value: ((i + 1) * 10) + j + 1 });
+    }
+}
+
 module.exports = {
     name: 'add_temp_channel',
     description: 'Créer un salon temporaire pour des discussions sur un module.',
@@ -12,61 +20,35 @@ module.exports = {
         name: 'groupe',
         description: 'Le groupe qui peut voir le channel',
         required: true,
-        choices: [
-            {
-                name: "Tous",
-                value: 0
-            },
-            {
-                name: "PSN1",
-                value: 10
-            },
-            {
-                name: "PSN2",
-                value: 20
-            },
-            {
-                name: "PSN2 Groupe 1",
-                value: 21
-            },
-            {
-                name: "PSN2 Groupe 2",
-                value: 22
-            }
-        ]
+        choices: choices
     }],
     async runSlash(client, interaction) {
         const inputNom = interaction.options.getString('nom');
         const inputGroupe = interaction.options.getInteger('groupe');
-        let permission = [{ id: '961267363808944169', deny: ['VIEW_CHANNEL'] }];
-        switch (inputGroupe){
-            case 10:
-                if (!interaction.member.roles.cache.has('991311405951234208')) return interaction.reply({ content: `Vous n'avez pas la permission de faire cette action !`, ephemeral: true });
-                permission.push({ id: '991311405951234208', allow: ['VIEW_CHANNEL'] });
-                break;
-            case 20:
-                if (!interaction.member.roles.cache.has('991310981324091472') && !interaction.member.roles.cache.has('991311841840083054')) return interaction.reply({ content: `Vous n'avez pas la permission de faire cette action !`, ephemeral: true });
-                permission.push(
-                    { id: '991310981324091472', allow: ['VIEW_CHANNEL'] },
-                    { id: '991311841840083054', allow: ['VIEW_CHANNEL'] }
-                );
-                break;
-            case 21:
-                if (!interaction.member.roles.cache.has('991310981324091472')) return interaction.reply({ content: `Vous n'avez pas la permission de faire cette action !`, ephemeral: true });
-                permission.push({ id: '991310981324091472', allow: ['VIEW_CHANNEL'] });
-                break;
-            case 22:
-                if (!interaction.member.roles.cache.has('991311841840083054')) return interaction.reply({ content: `Vous n'avez pas la permission de faire cette action !`, ephemeral: true });
-                permission.push({ id: '991311841840083054', allow: ['VIEW_CHANNEL'] });
-                break;
-            case 0:
-                permission.push(
-                    { id: '991311405951234208', allow: ['VIEW_CHANNEL'] },
-                    { id: '991310981324091472', allow: ['VIEW_CHANNEL'] },
-                    { id: '991311841840083054', allow: ['VIEW_CHANNEL'] }
-                );
-                break;
+        let permission = [{ id: process.envVar.discord.everyoneRole, deny: ['VIEW_CHANNEL'] }];
 
+        if (Math.floor(inputGroupe/10) !== 0 && inputGroupe%10 !== 0){
+            if (!interaction.member.roles.cache.has(process.envVar.classes[Math.floor(inputGroupe/10) - 1].adminsRole[inputGroupe%10 - 1])) return interaction.reply({ content: `Vous n'avez pas la permission de faire cette action !`, ephemeral: true });
+            
+            permission.push({ id: process.envVar.classes[Math.floor(inputGroupe/10) - 1].groupesRole[inputGroupe%10 - 1], allow: ['VIEW_CHANNEL'] });
+        }
+        else if (inputGroupe !== 0){
+            let auth = 0;
+
+            for (let i = 0; i < process.envVar.classes[Math.floor(inputGroupe/10) - 1].adminsRole.length; i++) {
+                if (interaction.member.roles.cache.has(process.envVar.classes[Math.floor(inputGroupe/10) - 1].adminsRole[i])) auth += 1;
+                
+                permission.push({ id: process.envVar.classes[Math.floor(inputGroupe/10) - 1].groupesRole[i], allow: ['VIEW_CHANNEL'] });
+            }
+            
+            if (auth === 0) return interaction.reply({ content: `Vous n'avez pas la permission de faire cette action !`, ephemeral: true });
+        }
+        else{
+            for (let i = 0; i < process.envVar.classes.length; i++) {
+                for (let j = 0; j < process.envVar.classes[i].groupesRole.length; j++) {
+                    permission.push({ id: process.envVar.classes[i].groupesRole[j], allow: ['VIEW_CHANNEL'] });
+                }
+            }
         }
         interaction.guild.channels.create(inputNom, { 
             type: 'GUILD_TEXT', 
