e057ea19e9dcbde04076960baf708d2525805d13
Louis Ducruet <louis.ducruet@epsi.fr>
2022-08-15 22:07:38 +0200

    Déconnexion des serveurs non valide #101

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,6 +7,14 @@ module.exports = {
     async execute(client) {
         // Log la disponibilité du bot
         logger.info(`${client.user.username} est en ligne !`);
+        // Déconnexion des serveur non valide
+        client.guilds.cache.forEach(guild => {
+            if (guild.id !== process.envVar.discord.guild){
+                logger.debug(`Déconnexion du serveur : ${guild.name}`)
+                guild.leave()
+            }
+            
+        })
         // Définition du serveur dans lequel les commandes sont ajoutées
         const devGuild = await client.guilds.cache.get(process.envVar.discord.guild);
         // Ajout des commandes au serveur
