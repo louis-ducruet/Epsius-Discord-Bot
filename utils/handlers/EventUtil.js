840e6b402966b2febd5322055b59bfb55a6ea762
Louis Ducruet <louis.ducruet@epsi.fr>
2022-09-15 08:54:23 +0200

    [BUG] : fix /add_temp_channel log message #142

--- utils/handlers/EventUtil.js
+++ utils/handlers/EventUtil.js
@@ -24,5 +24,5 @@ module.exports = async client => {
 
 const eventList = [
     'ready', 'resumed', 'voiceServerUpdate', 'userUpdate', 'applicationCommandCreate', 
-    'applicationCommandUpdate', 'applicationCommandDelete', 'interactionCreate', 'messageCreate'
+    'applicationCommandUpdate', 'applicationCommandDelete', 'interactionCreate', 'messageCreate', 'guildScheduledEventCreate'
 ];
